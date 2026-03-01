ff7250e62ccb996b761d73f4878316626c072dea
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2026-03-01 18:00:00 +0000

    feat(middleware): add ingestion middleware v2

---
+++ middleware/ingestion_middleware_v2_fill.py
@@ -0,0 +1,16 @@
+"""Ingestion middleware v2."""
+import time
+from typing import Any, Callable
+
+
+class IngestionMiddlewareV2:
+    def __init__(self):
+        self._count = 0
+
+    def __call__(self, handler: Callable) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            self._count += 1
+            start = time.monotonic()
+            result = handler(*args, **kwargs)
+            return result
+        return wrapper
