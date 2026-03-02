fd50e6f91abb09f879f6c992da74f854b785cae0
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2026-03-02 14:10:00 +0000

    feat(middleware): add classification middleware v2

---
+++ middleware/classification_middleware_v2_fill.py
@@ -0,0 +1,16 @@
+"""Classification middleware v2."""
+import time
+from typing import Any, Callable
+
+
+class ClassificationMiddlewareV2:
+    def __init__(self):
+        self._count = 0
+
+    def __call__(self, handler: Callable) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            self._count += 1
+            start = time.monotonic()
+            result = handler(*args, **kwargs)
+            return result
+        return wrapper
